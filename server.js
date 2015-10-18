f8b2c2e99fe028640ff4cc4c0505eb33193c54d2
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2015-10-18 15:10:18 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express"),
+    app = express(),
+    path = require("path"),
+    bodyParser = require("body-parser");
+
+// CONFIG //
+// set ejs as view engine
+app.set('view engine', 'ejs');
+// serve js & css files
+app.use("/static", express.static("public"));
+// body parser config to accept our datatypes
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/",function(req,res){
+	res.send("hola!");
+});
+
+
+
+
+
+
+
+
+
+var server = app.listen(process.env.PORT || 3000, function (){
+  console.log("listening on port 3000");
+});
